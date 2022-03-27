5d60d8331f86ab232993153b9b921f4ab4ce41bf
GusRendle <gus.rendle@protonmail.com>
2022-03-27 19:10:48 +0100

    Added LocationBindings, fixed toolbars

--- app/src/main/java/com/rendle/locationapp/AddLocationActivity.kt
+++ app/src/main/java/com/rendle/locationapp/AddLocationActivity.kt
@@ -2,10 +2,27 @@ package com.rendle.locationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rendle.locationapp.databinding.ActivityAddLocationBinding
+
+private lateinit var binding: ActivityAddLocationBinding
 
 class AddLocationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Calls parent constructor
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_location)
+        //Creates objects from XML file
+        binding = ActivityAddLocationBinding.inflate(layoutInflater)
+        //Sets XML view of this class
+        setContentView(binding.root)
+
+        //Sets the Tool Bar as a Support Action Bar
+        setSupportActionBar(binding.toolbarAddLocation)
+        // Adds the back button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Setting the click event to the back button
+        binding.toolbarAddLocation.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
--- app/src/main/java/com/rendle/locationapp/MainActivity.kt
+++ app/src/main/java/com/rendle/locationapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.rendle.locationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rendle.locationapp.databinding.ActivityMainBinding
+
+//Uses a Data Binding to refer to objects by their XML IDs
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Calls parent constructor
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Creates objects from XML file
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //Sets XML view of this class
+        setContentView(binding.root)
+
+        //Sets the Tool Bar as a Support Action Bar
+        setSupportActionBar(binding.toolbarMain)
+        // Adds the back button
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        binding.fabAddLocation.setOnClickListener {
+            val intent = Intent(this, AddLocationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
